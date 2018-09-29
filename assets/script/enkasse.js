4f95fe5486f31bc1f9c8ef738197d5e750d704c1
theschitz <theodore@theschitz.se>
2018-09-29 12:16:47 +0200

    Removed debugging lines.

--- assets/script/enkasse.js
+++ assets/script/enkasse.js
@@ -1,13 +1,11 @@
 function menuToggle(part) {
     hideAllWithClass('page-part');
-    console.debug(part.innerHTML);
     part = part.innerHTML.toLowerCase() + '-part';
     show = document.getElementById(part);
     show.style.display = 'block';    
 }
 function hideAllWithClass(className) {
     var elements = document.getElementsByClassName(className);
-    console.debug(elements);
     for (const key in elements) {
         if (elements.hasOwnProperty(key)) {
             const element = elements[key];
